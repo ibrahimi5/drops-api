52a2aa729490d1b8f2cfa46effdb62a6d38d9fd0
I.Ibrahim <ilyasibrahim005@gmail.com>
2025-12-17 16:47:50 +0000

    Added connection to frontend

--- project/settings.py
+++ project/settings.py
@@ -46,7 +46,8 @@ INSTALLED_APPS = [
     'posts',
     'categories',
     'comments',
-    'rest_framework'
+    'rest_framework',
+    'corsheaders'
 ]
 
 REST_FRAMEWORK = {
@@ -66,6 +67,7 @@ SIMPLE_JWT = {
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -145,3 +147,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:5173',
+    ]
