77dc2ec9490cb31ef62db361275d5a3298baab34
I.Ibrahim <ilyasibrahim005@gmail.com>
2025-12-19 12:26:21 +0000

    Added image field to post model

--- posts/models.py
+++ posts/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     body = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+    image = models.URLField(blank=True)
     categories = models.ManyToManyField(
         to='categories.Category',
         related_name='post_categories'
